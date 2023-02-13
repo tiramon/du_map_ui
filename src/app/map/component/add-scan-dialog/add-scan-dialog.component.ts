2fd7e8d948bc581b089978f85415840ebbb4add1
tiramon <github@checkspam.de>
2023-02-13 22:57:12 +0100

    add adding scans by json data

--- src/app/map/component/add-scan-dialog/add-scan-dialog.component.ts
+++ src/app/map/component/add-scan-dialog/add-scan-dialog.component.ts
@@ -1,4 +1,13 @@
-import { Component, EventEmitter, Inject, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Inject,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { faDiscord, faJava } from '@fortawesome/free-brands-svg-icons';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
@@ -10,7 +19,7 @@ import { RequestService } from 'src/app/map/service/request.service';
 @Component({
   selector: 'dumap-add-scan-dialog',
   templateUrl: './add-scan-dialog.component.html',
-  styleUrls: ['./add-scan-dialog.component.scss']
+  styleUrls: ['./add-scan-dialog.component.scss'],
 })
 export class AddScanDialogComponent implements OnInit, OnChanges {
   faDownload = faDownload;
@@ -24,12 +33,14 @@ export class AddScanDialogComponent implements OnInit, OnChanges {
   currentPlanetId: number;
 
   @Output()
-  endModal  = new EventEmitter();
+  endModal = new EventEmitter();
 
   ocrResult: string;
 
+  jsonResult: string;
+
   tab = 1;
-  error  = [];
+  error = [];
 
   scan: Scan;
   ores: string[];
@@ -47,7 +58,7 @@ export class AddScanDialogComponent implements OnInit, OnChanges {
   ) {
     this.ores = oreNames;
     this.scan = new Scan();
-   }
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.clearScan();
@@ -58,10 +69,12 @@ export class AddScanDialogComponent implements OnInit, OnChanges {
   }
 
   get maxTiles(): number {
-    return this.maxTilesForPlanet(this.planets.find((p: {name: string}) => p.name === this.scan.planet));
+    return this.maxTilesForPlanet(
+      this.planets.find((p: { name: string }) => p.name === this.scan.planet)
+    );
   }
 
-  private maxTilesForPlanet(planet: {gp}): number {
+  private maxTilesForPlanet(planet: { gp }): number {
     return planet.gp * planet.gp * 3 * 10 + 2;
   }
   /**
@@ -73,7 +86,9 @@ export class AddScanDialogComponent implements OnInit, OnChanges {
     this.scan.time = this.nowUTC();
     this.scan.ores = {};
     if (this.currentPlanetId) {
-      this.scan.planet = this.planets.find(p => p.id === this.currentPlanetId).name;
+      this.scan.planet = this.planets.find(
+        p => p.id === this.currentPlanetId
+      ).name;
     } else {
       this.scan.planet = null;
     }
@@ -83,7 +98,7 @@ export class AddScanDialogComponent implements OnInit, OnChanges {
   nowUTC(): Date {
     const date = new Date();
     const offset = date.getTimezoneOffset();
-    return new Date(date.getTime() - (offset * 60 * 1000));
+    return new Date(date.getTime() - offset * 60 * 1000);
   }
 
   /**
@@ -129,7 +144,7 @@ export class AddScanDialogComponent implements OnInit, OnChanges {
       this.toastr.error('Your scan input contained errors.');
       return;
     }
-    //fix time
+    // fix time
     const now = new Date();
     if (typeof this.scan.time === 'string') {
       console.log('fix date');
@@ -156,12 +171,44 @@ export class AddScanDialogComponent implements OnInit, OnChanges {
       },
       error => {
         if (error.status === 400) {
-          this.error.push('Invalid Data send, plz write 123000 instead of 123kL');
+          this.error.push(
+            'Invalid Data send, plz write 123000 instead of 123kL'
+          );
         }
       }
     );
   }
 
+  parseAndSaveScan(close: boolean) {
+    this.clearScan();
+    this.error = [];
+
+    this.scan = this.parseJsonResponse(this.jsonResult, this.scan);
+    if (this.scan && this.error.length === 0) {
+      this.requestService.saveScan(this.scan).then(
+        response => {
+          if (response) {
+            this.toastr.success('Your scan was successfully saved.');
+            this.eventService.scanAdded.emit(this.scan);
+          }
+          if (close && response) {
+            this.endModal.emit();
+          } else {
+            this.clearScan();
+            this.jsonResult = undefined;
+          }
+          this.eventService.loading.emit(false);
+        },
+        error => {
+          if (error.status === 400) {
+            this.error.push(
+              'Invalid Data send, plz write 123000 instead of 123kL'
+            );
+          }
+        }
+      );
+    }
+  }
   /**
    * Parse the OCR scan and write the parsed content to the form
    */
@@ -170,6 +217,18 @@ export class AddScanDialogComponent implements OnInit, OnChanges {
     this.error = [];
 
     const result = this.parseResponse(this.ocrResult, this.scan);
+    if (result && this.error.length === 0) {
+      this.tab = 1;
+      this.toastr.success('Your scan was successfully parsed.');
+    }
+    this.formTouched = true;
+  }
+
+  fillJsonForm() {
+    this.clearScan();
+    this.formTouched = true;
+    this.error = [];
+    const result = this.parseJsonResponse(this.jsonResult, this.scan);
     if (result && this.error.length === 0) {
       this.tab = 1;
       this.toastr.success('Your scan was successfully parsed.');
@@ -202,7 +261,7 @@ export class AddScanDialogComponent implements OnInit, OnChanges {
     }
     console.log('first split', splittedInput);
     const map = {};
-    splittedInput.map(l => l.split(/\t/)).forEach(ls => map[ls[0]] = ls[1]);
+    splittedInput.map(l => l.split(/\t/)).forEach(ls => (map[ls[0]] = ls[1]));
     console.log(map);
 
     let keys = Object.keys(map);
@@ -212,12 +271,17 @@ export class AddScanDialogComponent implements OnInit, OnChanges {
     const territorykey = this.bestMatch('TERRITORY ID', keys);
     const agekeys = this.bestMatch('AGE', keys);
 
-    const bestSystemMatch = this.bestMatch(map[systemkey], this.planets.map(p => p.name));
+    const bestSystemMatch = this.bestMatch(
+      map[systemkey],
+      this.planets.map(p => p.name)
+    );
     scan.planet = bestSystemMatch;
     this.refreshCurrentOres();
 
     if (+map[territorykey] != map[territorykey]) {
-      this.error.push(`could not parse the value of TERRITORY ID-> ${map[territorykey]} is not numeric`);
+      this.error.push(
+        `could not parse the value of TERRITORY ID-> ${map[territorykey]} is not numeric`
+      );
     }
     scan.tileId = +map[territorykey];
 
@@ -242,25 +306,34 @@ export class AddScanDialogComponent implements OnInit, OnChanges {
 
     age *= 60 * 1000;
     */
-    scan.time =  new Date();
+    scan.time = new Date();
     keys = this.remove(keys, agekeys); // min hrs d
     console.log(keys);
     for (const key of keys) {
-      const out = this.bestMatch(key, this.oreNames.map(o => o.name));
+      const out = this.bestMatch(
+        key,
+        this.oreNames.map(o => o.name)
+      );
       let amount = map[key];
       let splitted = amount.split(' ');
       if (splitted.length > 2) {
         amount = amount.replace(' ', '');
         splitted = amount.split(' ');
       } else if (splitted.length < 2) {
-        this.error.push(`could not parse the value of ${key} -> ${amount} it's probably missing a whitespace between value and unit '123L' instead of '123 L'`);
+        this.error.push(
+          `could not parse the value of ${key} -> ${amount} it's probably missing a whitespace between value and unit '123L' instead of '123 L'`
+        );
       }
       if (splitted[1].length > 2) {
-        this.error.push(`could not parse the value of ${key} -> ${amount} it's probably missing a whitespace between value and unit '1 123L' instead of '1123 L'`);
+        this.error.push(
+          `could not parse the value of ${key} -> ${amount} it's probably missing a whitespace between value and unit '1 123L' instead of '1123 L'`
+        );
       }
       const oreUnit = this.bestMatch(splitted[1], ['L', 'kL']);
       if (+splitted[0] != splitted[0]) {
-        this.error.push(`could not parse the value of ${key} -> ${splitted[0]} is not numeric`);
+        this.error.push(
+          `could not parse the value of ${key} -> ${splitted[0]} is not numeric`
+        );
       }
       let value = +splitted[0].trim();
 
@@ -275,10 +348,65 @@ export class AddScanDialogComponent implements OnInit, OnChanges {
     return scan;
   }
 
-  private bestMatch( input: string, possibleValues: string[]) {
+  parseJsonResponse(text: string, scan: Scan): Scan {
+    let parsed: {
+      planetId: number;
+      tileId: number;
+      territoryPool: { itemId: number; maxMiningRate: number }[];
+    };
+    try {
+      parsed = JSON.parse(text);
+    } catch (e) {
+      this.error.push('Invalid JSON');
+      return undefined;
+    }
+
+    scan.time = new Date();
+    scan.planet = this.planets.filter(
+      planet => planet.planetId === +parsed.planetId
+    )[0]?.name;
+    if (!scan.planet) {
+      this.error.push(`invalid planetId '${parsed.planetId}'`);
+      return undefined;
+    }
+
+    scan.tileId = parsed.tileId;
+    if (!scan.tileId) {
+      this.error.push('invalid tileId');
+    }
+    if (scan.tileId != +scan.tileId) {
+      this.error.push('invalid tileId');
+    }
+    if (scan.tileId < 0) {
+      this.error.push('invalid tileId, min value is 0');
+    }
+    if (scan.tileId >= this.maxTiles) {
+      this.error.push(`invalid tileId, max value is ${this.maxTiles}`);
+    }
+
+    for (const pool of parsed.territoryPool) {
+      const oreName = this.oreNames.filter(
+        ore => ore.itemId === +pool.itemId
+      )[0]?.name;
+      if (!oreName) {
+        this.error.push(`invalid itemId '${pool.itemId}'`);
+      }
+
+      scan.ores[oreName] = pool.maxMiningRate;
+      if (scan.ores[oreName] != +scan.ores[oreName]) {
+        this.error.push(
+          `invalid maxMiningRate '${pool.maxMiningRate}' for itemId ${pool.itemId}`
+        );
+      }
+    }
+
+    return scan;
+  }
+
+  private bestMatch(input: string, possibleValues: string[]) {
     let best = Infinity;
     let bestString = '';
-    for (const  value of possibleValues) {
+    for (const value of possibleValues) {
       const v = this.calculate(value, input);
       if (v < best) {
         best = v;
@@ -292,14 +420,20 @@ export class AddScanDialogComponent implements OnInit, OnChanges {
     const dp = [];
 
     for (let i = 0; i <= x.length; i++) {
-      if (!dp[i]) { dp[i] = []; }
+      if (!dp[i]) {
+        dp[i] = [];
+      }
       for (let j = 0; j <= y.length; j++) {
         if (i === 0) {
           dp[i][j] = j;
         } else if (j === 0) {
           dp[i][j] = i;
         } else {
-          dp[i][j] = this.min(dp[i - 1][j - 1] + this.costOfSubstitution(x[i - 1], y[j - 1]), dp[i - 1][j] + 1, dp[i][j - 1] + 1);
+          dp[i][j] = this.min(
+            dp[i - 1][j - 1] + this.costOfSubstitution(x[i - 1], y[j - 1]),
+            dp[i - 1][j] + 1,
+            dp[i][j - 1] + 1
+          );
         }
       }
     }
@@ -310,7 +444,7 @@ export class AddScanDialogComponent implements OnInit, OnChanges {
     return a === b ? 0 : 1;
   }
 
-  private min( n1: number, n2: number, n3: number) {
+  private min(n1: number, n2: number, n3: number) {
     return Math.min(n1, n2, n3);
   }
 }
